940ad91ece6ac3bcfe0290b434f4838383f02714
Victor Martinez <victoryanmartinez@gmail.com>
2024-05-08 15:24:03 -0300

    adding solutions

---
+++ beecrowd1184.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main() {
+    double n, s;
+    char sm;
+    s = 0;
+    double mat[12][12];
+    scanf("%c", &sm);
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            scanf("%lf", &n);
+            mat[i][j] = n;
+            if (j < i) {
+                s+= mat[i][j];
+            }
+        }
+    }
+    if (sm == 'S') {
+        printf("%.1lf\n", s);
+    } else {
+        printf("%.1lf\n", s/66);
+    }
+    return 0;
+}
